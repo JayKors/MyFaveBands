f5377117ccc3e2a1adeb2ca2fd8a9ab59ad2e9c3
Jay Kors <JAKors@student.rockvalleycollege.edu>
2019-09-22 21:35:56 -0500

    New Repo Commit - JK

--- MyFaveBands/DetailViewController.swift
+++ MyFaveBands/DetailViewController.swift
@@ -9,32 +9,42 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    //********1) Remove label from DetailViewController
+    //            -  add an ImageView
+    //            -  create outlet of imageview and name productImageView
+    
+    //********2) Comment out (1) line below
+    //    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        if detailItem != nil {
+            //********3) Update (6) lines of code below
+            if let photo = productImageView {
+                photo.image = UIImage(named:detailItem!)
             }
+        }else{
+            productImageView.image = UIImage(named:"Favorite Bands.jpg")
+            title = "My Bands"
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         configureView()
     }
-
-    var detailItem: NSDate? {
+    
+    //********4) Update next line of code below
+    var detailItem: String? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
 
